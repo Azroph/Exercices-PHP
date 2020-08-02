0f7d48c375acddffc08e12eec459c972bfc491e7
Azroph <56892575+Azroph@users.noreply.github.com>
2020-08-02 15:38:56 +0200

    Add files via upload

---
+++ Hello-World.php
@@ -0,0 +1,19 @@
+<h1>Premier programme php</h1>
+<?php
+//Commentaire sur une ligne
+echo "Hello World !<br>";
+echo "Une deuxième ligne";
+/*Commentaire
+sur
+plusieurs
+ligne 
+echo => Cette ligne n'est pas interprétée*/
+echo "Cette ligne n'est pas commentée";
+
+$maChaine = "une chaine de caractère";
+$compteur = 24;
+$decimal = 17.3;
+
+var_dump($maChaine,$compteur,$decimal);
+
+?>
