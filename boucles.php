bab07cb80680a15a8464aba5c8e626efabe8807c
Azroph <56892575+Azroph@users.noreply.github.com>
2020-08-05 13:04:20 +0200

    Add files via upload

---
+++ boucles.php
@@ -0,0 +1,18 @@
+<?php
+
+$repetition = 0;
+
+while ($repetition < 10)
+{
+   echo "<p>je ne dois pas copier sur mon voisin " . $repetition . " fois.</p>";
+   $repetition++;
+}
+
+for ($repetition = 0 ; $repetition  != 20 ; $repetition++) {
+
+echo "<p>je ne dois pas copier sur mon voisin " . $repetition . " fois.</p>";
+
+}
+
+
+?>
